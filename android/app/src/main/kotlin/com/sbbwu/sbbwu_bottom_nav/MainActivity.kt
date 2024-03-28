91fe451aacce8f9fb2ae8273d4f1f34b8f021c22
Abid <abidroid1@gmail.com>
2024-03-28 15:20:09 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/sbbwu/sbbwu_bottom_nav/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sbbwu.sbbwu_bottom_nav
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/parent_screen.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class ParentScreen extends StatefulWidget {
+  const ParentScreen({super.key});
+
+  @override
+  State<ParentScreen> createState() => _ParentScreenState();
+}
+
+class _ParentScreenState extends State<ParentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
