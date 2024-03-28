a9f06c06bad9b589c30e41046a5a45c1a7409306
Abid <abidroid1@gmail.com>
2024-03-28 16:05:44 +0500

    drawer added

--- lib/parent_screen.dart
+++ lib/parent_screen.dart
@@ -12,7 +12,6 @@ class ParentScreen extends StatefulWidget {
 }
 
 class _ParentScreenState extends State<ParentScreen> {
-
   // state variable
   int selectedIndex = 0;
 
@@ -26,26 +25,76 @@ class _ParentScreenState extends State<ParentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: Container(
+                color: Colors.blue,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.ac_unit_sharp,
+                      size: 80,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      ' SHAHEED BENAZIR BHUTTO WOMEN UNIVERSITY',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            ListTile(leading: Icon(Icons.person),
+              title: const Text('Admin'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: (){},
+            ),
+            ListTile(leading: Icon(Icons.subject),
+              title: const Text('Faculties'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: (){},
+            ),
+            ListTile(leading: Icon(Icons.add_card_rounded),
+              title: const Text('Academics'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: (){},
+            ),
+            Divider(),
+            ListTile(leading: Icon(Icons.star_outline),
+              title: const Text('Rate us'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: (){},
+            ),
+
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-
         currentIndex: selectedIndex,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             selectedIndex = index;
           });
         },
         items: const [
-           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.ac_unit_sharp), label: 'About Us'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit_sharp), label: 'About Us'),
           BottomNavigationBarItem(icon: Icon(Icons.phone), label: 'Contact Us'),
-          BottomNavigationBarItem(icon: Icon(Icons.star_outline), label: 'Stories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star_outline), label: 'Stories'),
         ],
       ),
       appBar: AppBar(
         title: const Text('Bottom Nav Demo'),
       ),
-
       body: pages[selectedIndex],
     );
   }
